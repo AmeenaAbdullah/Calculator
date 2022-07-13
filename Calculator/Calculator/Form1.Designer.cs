db5be2ef292e869b87556dc238467c3445fe23fb
Ameena <99495118+AmeenaAbdullah@users.noreply.github.com>
2022-07-13 15:54:12 +0500

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.maskedTextBox1);
+            this.panel1.Location = new System.Drawing.Point(84, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(460, 645);
+            this.panel1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 38);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(660, 722);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem standardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scientificToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programmerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem statisticsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dgitGroupingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem basicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unitConversionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dateCalculationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewHelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutCalculatorToolStripMenuItem;
+        private System.Windows.Forms.Label Calculator;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,429 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public string value1 = "0", value2 = "0";
+        public int flag = 0;
+        public int eval = 0;
+       
+        public Form1()
+        {
+            InitializeComponent();
+       
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button_Click(button1);
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button_Click(button8);
+
+
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button_Click(button5);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button_Click(button6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button_Click(button7);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            button_Click(button16);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            button_Click(button15);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            button_Click(button14);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            button_Click(button12);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            button_Click(button11);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button_Click(button10);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button_Click(button2);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button_Click(button3);
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button_Click(button9);
+
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            button_Click(button13);
+
+
+        }
+        private void button21_Click(object sender, EventArgs e)
+        {
+            button_Click(button21);
+
+
+        }
+        private void button_Click(Button btn)
+        {
+            if (richTextBox1.Text == "0") richTextBox1.Text = ""; 
+            if (btn.Text == "+" || btn.Text == "-" || btn.Text == "*" || btn.Text == "/" )
+            {
+                flag = 0;
+                if (richTextBox2.Text != "" && richTextBox2.Text != "")
+                {
+                    if (richTextBox2.Text[0] == '+' || richTextBox2.Text[0] == '-' ||  richTextBox2.Text[0] == '*' || richTextBox2.Text[0] == '/')
+                    {
+                        
+                        calculate(btn,value1,value2);
+                       
+                    }
+                    else if (richTextBox2.Text[0] == '=')
+                    {
+                      value2 = richTextBox1.Text;
+                      richTextBox2.Text = btn.Text + richTextBox1.Text;
+                    }
+                }
+                else
+                {
+                    value2 = richTextBox1.Text;
+                    richTextBox2.Text = btn.Text + richTextBox1.Text;
+                    
+                }
+
+            }
+            else if(btn.Text == "=")
+            {
+                calculate(btn, value1, value2);
+                flag = 0;
+
+            }
+            else 
+             {
+                eval = 0;
+                if (richTextBox2.Text != "")
+                {
+                    if (richTextBox2.Text[0] == '+' || richTextBox2.Text[0] == '-' || richTextBox2.Text[0] == '*' || richTextBox2.Text[0] == '/')
+                    {
+                        //  calculate(btn);
+                        if (flag == 0)
+                        {
+                            richTextBox1.Text = btn.Text; flag = 1;
+                            
+                        }
+                        else richTextBox1.Text += btn.Text;
+
+                    }
+                    else
+                    {
+                        if (flag == 0)
+                        {
+                            richTextBox1.Text = btn.Text; flag = 1;
+
+                        }else
+                        richTextBox1.Text += btn.Text;
+                    }
+                }
+                else
+                {
+                   
+                    richTextBox1.Text += btn.Text;
+                }
+               
+               
+
+            }
+            
+           
+        }
+        public void calculate(Button btn,string str1,string str)
+        {
+            //  if (btn.Text != "=") eval = 1;
+            if (eval == 0)
+            {
+                if (btn.Text != "=") eval = 1;
+                string str2 = richTextBox1.Text;
+                Char c = richTextBox2.Text[0];
+                if (c == '+')
+                {
+                    var r = Convert.ToDouble(str) + Convert.ToDouble(str2);
+                
+                    if (r < 0)
+                    {
+                        var r2 = Math.Abs(r);
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "+" + str2;
+                            richTextBox1.Text = r2.ToString() + "-";
+                        }
+                        else
+                        {
+                            string s = btn.Text + r2.ToString() + "-";
+                            richTextBox2.Text = s;
+                        }
+
+
+                    }
+                    else
+                    {
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "+" + str2;
+                            richTextBox1.Text = r.ToString();
+                        }
+                        else
+                        {
+                            string s = btn.Text + r.ToString();
+                            richTextBox2.Text = s;
+                        }
+                    }
+
+                    value2 = r.ToString();
+                }
+                else if (c == '-')
+                {
+                    var r = Convert.ToDouble(str) - Convert.ToDouble(str2); ;
+
+                    if (r < 0)
+                    {
+                        var r2 = Math.Abs(r);
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "-" + str2;
+                            richTextBox1.Text = r2.ToString() + "-";
+                        }
+                        else
+                        {
+                            string s = btn.Text + r2.ToString() + "-";
+                            richTextBox2.Text = s;
+                        }
+
+
+                    }
+                    else
+                    {
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "-" + str2;
+                            richTextBox1.Text = r.ToString();
+                        }
+                        else
+                        {
+                            string s = btn.Text + r.ToString();
+                            richTextBox2.Text = s;
+                        }
+                    }
+
+
+                    value2 = r.ToString();
+                }
+                else if (c == '/')
+                {
+                    var r = Convert.ToDouble(str) / Convert.ToDouble(str2);
+                    
+                    if (r < 0)
+                    {
+                        var r2 = Math.Abs(r);
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "/" + str2;
+                            richTextBox1.Text = r2.ToString() + "-";
+                        }
+                        else
+                        {
+                            string s = btn.Text + r2.ToString() + "-";
+                            richTextBox2.Text = s;
+                        }
+
+
+                    }
+                    else
+                    {
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "-" + str2;
+                            richTextBox1.Text = r.ToString();
+                        }
+                        else
+                        {
+                            string s = btn.Text + r.ToString();
+                            richTextBox2.Text = s;
+                        }
+                    }
+
+
+
+
+
+                    value2 = r.ToString();
+                }
+                else if (c == '*')
+                {
+                    var r = Convert.ToDouble(str) * Convert.ToDouble(str2);
+
+                    if (r < 0)
+                    {
+                        var r2 = Math.Abs(r);
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "*" + str2;
+                            richTextBox1.Text = r2.ToString() + "-";
+                        }
+                        else
+                        {
+                            string s = btn.Text + r2.ToString() + "-";
+                            richTextBox2.Text = s;
+                        }
+
+
+                    }
+                    else
+                    {
+                        if (btn.Text == "=")
+                        {
+                            richTextBox2.Text = "=" + str + "*" + str2;
+                            richTextBox1.Text = r.ToString();
+                        }
+                        else
+                        {
+                            string s = btn.Text + r.ToString();
+                            richTextBox2.Text = s;
+                        }
+                    }
+
+                    value2 = r.ToString();
+                }
+            }
+            else {
+               
+                var v = Convert.ToDouble(value2);
+                if(v>0)
+                richTextBox2.Text = btn.Text + value2 ;
+                else
+                {
+                    v = Math.Abs(v);
+                    richTextBox2.Text = btn.Text+ v +"-" ;
+                }
+              
+            } 
+
+
+
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "0";
+            richTextBox2.Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.Text = "0";
+            richTextBox2.Text = "";
+        }
+
+    
+
+       
+
+        public string reverseStr(string s)
+        {
+            string sr="";
+            for (int i=s.Length-1 ; i>=0 ; i--)
+            {
+               sr+=s[i];
+                
+            }
+            return sr;
+        }
+    }
+}
